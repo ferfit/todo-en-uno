63a8411249a660307ad0a45f02f85913ab9a8b74
Pereyra-Fernando <pereyra.r.fernando@gmail.com>
2021-10-06 12:04:38 -0200

    agrega seccion oferta y boton whatsapp

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// variable 
+var whatsapp = document.querySelector('.whatsapp');
+var consulta = document.querySelector('.consulta');
+
+// creamos un evento que escuche el scroll y al detectarlo se ejecute una funcion,
+document.addEventListener('scroll',function(){   
+    var posicion = window.scrollY;
+    if(posicion>1000){
+        whatsapp.classList.add('mostrarWhatsapp');
+    } else{
+        whatsapp.classList.remove('mostrarWhatsapp');
+    }
+
+    if(posicion>3000){
+        consulta.classList.add('mostrarConsulta');
+    } else{
+        consulta.classList.remove('mostrarConsulta');
+    }
+});
+
+
+
+
+
